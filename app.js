050b6ff21bff0266669fd50445cfa4d85e59110a
Katie Wood <woodkm11@gmail.com>
2016-11-17 16:35:36 -0500

    Temporary fix for image path issue using Imgur links.

--- app.js
+++ app.js
@@ -8,18 +8,29 @@ console.log(randomDuck());
 
 // Changes image of duck on click
 
+// function changeImage(imageId) {
+//   if (document.getElementById(imageId).src == "./images/duck-inactive.png") {
+//     document.getElementById(imageId).src = "./images/duck.png";
+//   } else if (document.getElementById(imageId).src == "./images/duck.png"){
+//     document.getElementById(imageId).src = "./images/duck-inactive.png";
+//   }
+// }
+
+
+
 function changeImage(imageId) {
-  if (document.getElementById(imageId).src == "./images/duck-inactive.png") {
-    document.getElementById(imageId).src = "./images/duck.png";
-  } else if (document.getElementById(imageId).src == "./images/duck.png"){
-    document.getElementById(imageId).src = "./images/duck-inactive.png";
+  if (document.getElementById(imageId).src == "http://i.imgur.com/GyAkOyQ.png") {
+    document.getElementById(imageId).src = "http://i.imgur.com/sAnalhL.png";
+  } else if (document.getElementById(imageId).src == "http://i.imgur.com/sAnalhL.png"){
+    document.getElementById(imageId).src = "http://i.imgur.com/GyAkOyQ.png";
   }
 }
 
-console.log(document.getElementById("duck2").src);
 
-changeImage("duck2");
+console.log(document.getElementById("duck5").src);
 
+changeImage("duck5");
+changeImage("duck9");
 
 // document.getElementById("duck3").src = "images/duck.png";
 
